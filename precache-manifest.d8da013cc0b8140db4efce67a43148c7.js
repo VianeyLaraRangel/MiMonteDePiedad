b875e0c5d4a9dd8bab0a12704bf39f60cc2f8068
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-02 19:21:34 -0600

    Updates

---
+++ precache-manifest.d8da013cc0b8140db4efce67a43148c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbe4ae7df600aabe6be56c8aa36bfbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e1c62e381548df170a1",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "81af2b94956acbbeebf7",
+    "url": "./static/js/2.cf223f6b.chunk.js"
+  },
+  {
+    "revision": "4e1c62e381548df170a1",
+    "url": "./static/js/main.0523232c.chunk.js"
+  },
+  {
+    "revision": "ad8db26528bd437f9b2f",
+    "url": "./static/js/runtime-main.ac0187f0.js"
+  }
+]);
