acc160b196cf340319cd6f9b73f66fdb7c738380
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-03 17:17:57 -0600

    Updates

---
+++ precache-manifest.91aaccfd833cc970f409f6198c2149ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9274e00cee368c7204c72c35cfa2fc24",
+    "url": "/MiMonteDePiedad/index.html"
+  },
+  {
+    "revision": "bf979f16381859dbb947",
+    "url": "/MiMonteDePiedad/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7d9e4570fde5e7778e2e",
+    "url": "/MiMonteDePiedad/static/css/main.52ee3a09.chunk.css"
+  },
+  {
+    "revision": "bf979f16381859dbb947",
+    "url": "/MiMonteDePiedad/static/js/2.ef41fb01.chunk.js"
+  },
+  {
+    "revision": "7d9e4570fde5e7778e2e",
+    "url": "/MiMonteDePiedad/static/js/main.f444a263.chunk.js"
+  },
+  {
+    "revision": "06446b4ce9f429cab839",
+    "url": "/MiMonteDePiedad/static/js/runtime-main.f18e4326.js"
+  },
+  {
+    "revision": "82233efd8585117f8a0caa454001a277",
+    "url": "/MiMonteDePiedad/static/media/monte-de-piedad-white.82233efd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2b137b066a43842a42d3dfb96b579f65.js"
+  "/MiMonteDePiedad/precache-manifest.91aaccfd833cc970f409f6198c2149ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MiMonteDePiedad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
