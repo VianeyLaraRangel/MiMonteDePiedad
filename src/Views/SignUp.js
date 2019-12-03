182a73c143fcabbf4b4cde90d44705adb8d1bc0f
MercedesDavila <mmdavila@outlook.com>
2019-12-03 06:04:23 -0600

    Agregamos vista de signup

--- src/Views/SignUp.js
+++ src/Views/SignUp.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import logo from "../img/monte_logo.png";
+
 
 class SignUp extends Component {
     render() {
         return (
-            <p>Aquí se renderiza el registro
-            hola
-            </p>
+            <div className="signup-screen">
+            <div className="backg-img">
+          <img src={logo} className="logo-signup" alt="logo" />    
+          <h1 className="greeting-login"></h1>
+          </div>
+          </div>
         );
     }
 }
