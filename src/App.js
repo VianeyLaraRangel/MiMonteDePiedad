af6048a0d43a1f351e8967d5e61e81ea0ff31ab2
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-03 15:49:43 -0600

    Modificaciones para deploy

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Home from './Views/Home';
 import LogIn from './Views/LogIn';
 import SignUp from './Views/SignUp';
 import CodeVerif from './Views/CodeVerif';
-// import FormRequest from './Views/Formulario';
-//import FormRequest from './Views/Formulario';
 import FormSobreTi from './Views/FormSobreTi';
 import FormPlanes from './Views/FormPlanes';
 import CreditProposal from './Views/CreditProposal';
@@ -19,18 +17,18 @@ import Credits from './Views/Credits';
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL} >
+      <Router>
         <Switch>
           <Route exact path='/' component={LogIn} />
-          <Route path='/SignUp' component={SignUp} />
-          <Route path='/CodeVerif' component={CodeVerif} />
-          <Route path='/inicio' component={Home} /> 
-          <Route path='/sobreti' component={FormSobreTi} />
-          <Route path='/tusplanes' component={FormPlanes} />
-          <Route path='/propuesta' component={CreditProposal} />
-          <Route path='/pago' component={Payment} />
-          <Route path='/tucredimonte' component={CrediMonte} />
-          <Route path='/tuscreditos' component={Credits} />
+          <Route exact path='/SignUp' component={SignUp} />
+          <Route exact path='/CodeVerif' component={CodeVerif} />
+          <Route exact path='/inicio' component={Home} /> 
+          <Route exact path='/sobreti' component={FormSobreTi} />
+          <Route exact path='/tusplanes' component={FormPlanes} />
+          <Route exact path='/propuesta' component={CreditProposal} />
+          <Route exact path='/pago' component={Payment} />
+          <Route exact path='/tucredimonte' component={CrediMonte} />
+          <Route exact path='/tuscreditos' component={Credits} />
         </Switch>
       </Router>
     );
--- src/Views/CrediMonte.js
+++ src/Views/CrediMonte.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import { Button } from 'reactstrap';
 
-
 import '../Components/Home/Home.css';
 import logo from '../img/monte_logo.png';
 
@@ -11,12 +10,10 @@ class CrediMonte extends Component {
     render() {
         return (
             <div className="home-box">
-                <Link to="/inicio" className="brand-box">
-                    <a className="back-icon">
-                        <i class="fas fa-chevron-left"></i>
-                        <img src={logo} className="home-logoCredi" alt="logo" />
-                    </a>
+                <Link to="/inicio" className="brand-box back-icon">
+                    <i className="fas fa-chevron-left"></i>
                 </Link>
+                <img src={logo} className="home-logoCredi" alt="logo" />
                 <p className="title-CrediMonte">Tu CrediMonte</p>
                 <section>
                     <Button color="info" className="btn-options">
